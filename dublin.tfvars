f48e2c0c9f98c4ce7d30a615a0a98ab7b4b633c4
Anusha <anushass96@gmail.com>
2021-10-16 19:00:07 +0100

    Task1 improved resiliency by implementing autoscaling

--- dublin.tfvars
+++ dublin.tfvars
@@ -3,4 +3,4 @@ region = "eu-west-1"
 vpc-cidr = "10.10.10.0/24"
 
 # feel free to change this subnet cidr block if required
-subnet-cidr-public = "10.10.10.0/27"
+subnet-cidr-public = ["10.10.10.0/27", "10.10.10.32/27", "10.10.10.64/28"]
--- main.tf
+++ main.tf
@@ -7,25 +7,32 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
   tags = {
     Project = "Tech Test"
-    Owner = "Anusha"
+    Owner   = "Anusha"
   }
 }
 
-resource "aws_subnet" "public-subnet" {
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+resource "aws_subnet" "public_subnet" {
+  count             = length(data.aws_availability_zones.available_zones.names)
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.subnet-cidr-public
-  availability_zone = "${var.region}a"
+  cidr_block        = var.subnet-cidr-public[count.index]
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
   tags = {
+    Name    = "Subnet${count.index}"
     Project = "Tech Test"
-    Owner = "Anusha"
+    Owner   = "Anusha"
   }
 }
+
 
 resource "aws_route_table" "public-subnet-route-table" {
   vpc_id = aws_vpc.vpc.id
   tags = {
     Project = "Tech Test"
-    Owner = "Anusha"
+    Owner   = "Anusha"
   }
 }
 
@@ -33,7 +40,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
     Project = "Tech Test"
-    Owner = "Anusha"
+    Owner   = "Anusha"
   }
 }
 
@@ -44,32 +51,53 @@ resource "aws_route" "public-subnet-route" {
 }
 
 resource "aws_route_table_association" "public-subnet-route-table-association" {
-  subnet_id      = aws_subnet.public-subnet.id
+  count = length(data.aws_availability_zones.available_zones.names)
+
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public-subnet-route-table.id
 }
+
+#data "aws_subnet" "subnet_values" {
+#  for_each = aws_subnet.public_subnet.id
+#  id       = each.value
+#}
 
 resource "aws_key_pair" "web" {
   public_key = sensitive(file(pathexpand(var.public_key)))
 }
 
-resource "aws_instance" "web-instance" {
-  ami                         = "ami-cdbfa4ab"
+resource "aws_launch_configuration" "launch-configuration" {
+  name                        = "Nginx-Config-Template"
+  image_id                    = "ami-cdbfa4ab"
   instance_type               = "t2.small"
-  vpc_security_group_ids      = [aws_security_group.web-instance-security-group.id]
-  subnet_id                   = aws_subnet.public-subnet.id
+  security_groups             = [aws_security_group.web-instance-security-group.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.web.key_name
-  tags = {
-    Project = "Tech Test"
-    Owner = "Anusha"
-  }
   user_data                   = <<EOF
 #!/bin/sh
 yum install -y nginx
 service nginx start
 EOF
-
 }
+
+
+resource "aws_autoscaling_group" "autoscalling_group_config" {
+  name                      = "Nginx_autoscale_group"
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity          = 1
+  force_delete              = true
+  vpc_zone_identifier       = [for s in aws_subnet.public_subnet : s.id]
+
+  launch_configuration = aws_launch_configuration.launch-configuration.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 
 resource "aws_security_group" "web-instance-security-group" {
   vpc_id = aws_vpc.vpc.id
@@ -95,11 +123,11 @@ resource "aws_security_group" "web-instance-security-group" {
   }
   tags = {
     Project = "Tech Test"
-    Owner = "Anusha"
+    Owner   = "Anusha"
   }
 }
 
-output "web_domain" {
-  value = aws_instance.web-instance.public_dns
-}
+#output "web_domain" {
+#  value = aws_instance.web-instance.public_dns
+#}
 
