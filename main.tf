f713a3d9a1c6869bd575da8a6914c63f623e7a00
Anusha <anushass96@gmail.com>
2021-10-17 16:08:15 +0100

    Task2 & Task3 code changes

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
+#specifying provider name along with region where the environment will be setup
 provider "aws" {
   region = var.region
 }
 
+#creating a new keypair
+resource "aws_key_pair" "web" {
+  public_key = sensitive(file(pathexpand(var.public_key)))
+}
+
+#Creating a VPC
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc-cidr
   enable_dns_hostnames = true
@@ -11,15 +18,18 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+#get all availability zones in the given region
 data "aws_availability_zones" "available_zones" {
   state = "available"
 }
 
-resource "aws_subnet" "public_subnet" {
-  count             = length(data.aws_availability_zones.available_zones.names)
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.subnet-cidr-public[count.index]
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+#Create subnets in all regions except one for resilient application deployment
+resource "aws_subnet" "private_subnet" {
+  count                   = length(data.aws_availability_zones.available_zones.names) - 1
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet-cidr-public[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
   tags = {
     Name    = "Subnet${count.index}"
     Project = "Tech Test"
@@ -27,15 +37,20 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-
-resource "aws_route_table" "public-subnet-route-table" {
-  vpc_id = aws_vpc.vpc.id
+#create subnet in one region for Bastion server
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet-cidr-public[length(data.aws_availability_zones.available_zones.names) - 1]
+  availability_zone       = data.aws_availability_zones.available_zones.names[length(data.aws_availability_zones.available_zones.names) - 1]
+  map_public_ip_on_launch = true
   tags = {
+    Name    = "Public Subnet"
     Project = "Tech Test"
     Owner   = "Anusha"
   }
 }
 
+#Create Internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
@@ -44,61 +59,36 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+#Create a route table
+resource "aws_route_table" "public-subnet-route-table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Project = "Tech Test"
+    Owner   = "Anusha"
+  }
+}
+
+#Create NAT rule
 resource "aws_route" "public-subnet-route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
   route_table_id         = aws_route_table.public-subnet-route-table.id
 }
 
-resource "aws_route_table_association" "public-subnet-route-table-association" {
-  count = length(data.aws_availability_zones.available_zones.names)
-
-  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+#Associate subnets to the route table
+resource "aws_route_table_association" "private-subnet-route-table-association" {
+  count          = length(data.aws_availability_zones.available_zones.names) - 1
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.public-subnet-route-table.id
 }
 
-#data "aws_subnet" "subnet_values" {
-#  for_each = aws_subnet.public_subnet.id
-#  id       = each.value
-#}
-
-resource "aws_key_pair" "web" {
-  public_key = sensitive(file(pathexpand(var.public_key)))
+#Associate Bastion subnet to route table
+resource "aws_route_table_association" "public-subnet-route-table-association1" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public-subnet-route-table.id
 }
 
-resource "aws_launch_configuration" "launch-configuration" {
-  name                        = "Nginx-Config-Template"
-  image_id                    = "ami-cdbfa4ab"
-  instance_type               = "t2.small"
-  security_groups             = [aws_security_group.web-instance-security-group.id]
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.web.key_name
-  user_data                   = <<EOF
-#!/bin/sh
-yum install -y nginx
-service nginx start
-EOF
-}
-
-
-resource "aws_autoscaling_group" "autoscalling_group_config" {
-  name                      = "Nginx_autoscale_group"
-  max_size                  = 2
-  min_size                  = 1
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
-  desired_capacity          = 1
-  force_delete              = true
-  vpc_zone_identifier       = [for s in aws_subnet.public_subnet : s.id]
-
-  launch_configuration = aws_launch_configuration.launch-configuration.name
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
+#Create security group for Nginx application
 resource "aws_security_group" "web-instance-security-group" {
   vpc_id = aws_vpc.vpc.id
 
@@ -108,6 +98,23 @@ resource "aws_security_group" "web-instance-security-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Project = "Tech Test"
+    Owner   = "Anusha"
+  }
+}
+
+#Create security group for Bastion server
+resource "aws_security_group" "bastion-security-group" {
+  vpc_id = aws_vpc.vpc.id
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -127,7 +134,72 @@ resource "aws_security_group" "web-instance-security-group" {
   }
 }
 
-#output "web_domain" {
-#  value = aws_instance.web-instance.public_dns
-#}
+#Creating security group for EC2 Bastion Host Access
+resource "aws_security_group" "bastion-EC2-SSH-group" {
+  vpc_id = aws_vpc.vpc.id
 
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion-security-group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Project = "Tech Test"
+    Owner   = "Anusha"
+  }
+}
+
+#Create launch configuration template for autoscaling group
+resource "aws_launch_configuration" "launch-configuration" {
+  name                        = "Nginx-Config-Template"
+  image_id                    = "ami-50c0ea46"
+  instance_type               = "t2.small"
+  security_groups             = [aws_security_group.web-instance-security-group.id, aws_security_group.bastion-EC2-SSH-group.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.web.key_name
+  user_data                   = <<EOF
+#!/bin/sh
+yum install -y nginx
+service nginx start
+EOF
+}
+
+#Create autoscaling group
+resource "aws_autoscaling_group" "autoscalling_group_config" {
+  name                      = "Nginx_autoscale_group"
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity          = 1
+  force_delete              = true
+  vpc_zone_identifier       = [for s in aws_subnet.private_subnet : s.id]
+
+  launch_configuration = aws_launch_configuration.launch-configuration.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#Create bastion EC2 instance
+resource "aws_instance" "Bastion-Host" {
+  ami           = "ami-50c0ea46"
+  instance_type = "t2.small"
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name = aws_key_pair.web.key_name
+  vpc_security_group_ids = [aws_security_group.bastion-security-group.id]
+  tags = {
+    Name    = "Bastion_Host_From_Terraform"
+    Project = "Tech Test"
+    Owner   = "Anusha"
+  }
+}
